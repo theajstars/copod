77f6874cb92b62e16edea3bd94b9cd9b20afb7eb
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-03-26 12:14:07 +0100

    Began data fetch from API

--- src/Components/CovidStatistics.jsx
+++ src/Components/CovidStatistics.jsx
@@ -5,6 +5,7 @@ import { Select } from "antd";
 import { Container } from "@mui/material";
 const { Option } = Select;
 import Globe from "../Assets/IMG/world-globe.png";
+import TestChart from "./TestChart";
 
 function handleChange(optionValue) {
   console.log("Value changed to: ", optionValue);
@@ -13,20 +14,82 @@ const globePrefix = <img src={Globe} className="country-select-image" />;
 
 export default function CovidStatistics() {
   const [countries, setCountries] = useState([]);
+  const [region, setRegion] = useState("everywhere");
+  const [timespan, setTimespan] = useState("");
+  const [dataType, setDataType] = useState("");
+
   useEffect(() => {
     axios
       .get(
         `https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json`
       )
       .then((res) => {
-        console.log(res);
         setCountries(res.data);
       });
   }, []);
+
+  useEffect(() => {
+    //Make request when parameters change
+    //If region is set to everywhere
+    var options = {};
+    if (region === "everywhere") {
+      options = {
+        method: "GET",
+        url: "https://covid-19-statistics.p.rapidapi.com/reports/total",
+        params: {
+          // date: timespan,
+        },
+        headers: {
+          "X-RapidAPI-Host": "covid-19-statistics.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "c25b202a81msh193143b5a7aefe3p11a9a3jsn7785bbb7b0fd",
+        },
+      };
+    } else {
+      //Region is set to a particular country
+      options = {
+        method: "GET",
+        url: "https://covid-19-statistics.p.rapidapi.com/reports/",
+        params: {
+          region_name: region,
+          date: timespan,
+        },
+        headers: {
+          "X-RapidAPI-Host": "covid-19-statistics.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "c25b202a81msh193143b5a7aefe3p11a9a3jsn7785bbb7b0fd",
+        },
+      };
+    }
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    axios.get();
+  }, [region, timespan, dataType]);
   return (
     <>
       <Container maxWidth="md">
         <div className="statistics-container flex-column">
+          <div className="flex-row stats-row">
+            <div className="stat-item">
+              <div className="stat-figure">45,000</div>
+              <small className="stat-name">Active Cases</small>
+            </div>
+            <div className="stat-item">
+              <div className="stat-figure">45,000</div>
+              <small className="stat-name">New Cases</small>
+            </div>
+            <div className="stat-item">
+              <div className="stat-figure">45,000</div>
+              <small className="stat-name">Total Deaths</small>
+            </div>
+          </div>
           <div className="flex-row statistics-select">
             <Select
               defaultValue="new-cases"
@@ -65,9 +128,11 @@ export default function CovidStatistics() {
               <Option value="all-time">All Time</Option>
               <Option value="last-30-days">Last 30 Days</Option>
               <Option value="two-weeks">Last 2 Weeks</Option>
+              <Option value="today">Today</Option>
             </Select>
           </div>
         </div>
+        <TestChart />
       </Container>
     </>
   );
